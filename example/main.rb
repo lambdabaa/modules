42b38b5da8275389e3784de5d3c8ce5b914c838b
lambdabaa <gareth@alumni.middlebury.edu>
2017-01-15 09:54:42 -0800

    Cleanup example/main.rb

--- example/main.rb
+++ example/main.rb
@@ -3,9 +3,5 @@ modular = import('./modular')
 # compatible with external globals-style ruby modules
 assert = import('test/unit/assertions')['Test::Unit::Assertions']
 
-p modular
-
 assert.assert_equal(modular[:add].call(2, 3, 2), 1)
-puts "PASS"
 assert.assert_equal(modular[:multiply].call(8, 7, 10), 6)
-puts "PASS"
